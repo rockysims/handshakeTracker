cc54287e9fab11a88e10485ace32ddd2c44e2bb6
Yona Appletree <yona@concentricsky.com>
2018-10-31 17:10:46 -0700

    Added scripts for building and deploying to github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-	production: true
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyCuTA9ocfGFN924xbOY2zEey_u09wuIKo0",
+		authDomain: "handshaketracker.firebaseapp.com",
+		databaseURL: "https://handshaketracker.firebaseio.com",
+		projectId: "handshaketracker",
+		storageBucket: "handshaketracker.appspot.com",
+		messagingSenderId: "257305979665"
+	}
 };
