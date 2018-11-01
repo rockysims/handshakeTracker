aedf0efd5419339a9004737c1d399ee67c7ce39f
Yona Appletree <yona@concentricsky.com>
2018-10-31 17:03:51 -0700

    Fixing style

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
 	production: false,
 	firebase: {
-		apiKey: 'AIzaSyCuTA9ocfGFN924xbOY2zEey_u09wuIKo0',
-		authDomain: 'handshaketracker.firebaseapp.com',
-		databaseURL: 'https://handshaketracker.firebaseio.com',
-		projectId: 'handshaketracker',
-		storageBucket: 'handshaketracker.appspot.com',
-		messagingSenderId: '257305979665'
+		apiKey: "AIzaSyCuTA9ocfGFN924xbOY2zEey_u09wuIKo0",
+		authDomain: "handshaketracker.firebaseapp.com",
+		databaseURL: "https://handshaketracker.firebaseio.com",
+		projectId: "handshaketracker",
+		storageBucket: "handshaketracker.appspot.com",
+		messagingSenderId: "257305979665"
 	}
 };
 
