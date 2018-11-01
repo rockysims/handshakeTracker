d0a57f04c7ba99ce185240cc361e5b20eca87f27
Yona Appletree <yona@concentricsky.com>
2018-11-01 12:45:51 -0700

    Updated code format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
+import { NgModule } from "@angular/core";
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { MatButtonModule, MatCheckboxModule } from "@angular/material";
-import {BrowserModule} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { environment } from "../environments/environment";
 
-import {AppRoutingModule} from "./app-routing.module";
-import {AppComponent} from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 const FIREBASE_IMPORTS = [
 	AngularFireModule.initializeApp(environment.firebase),
@@ -33,7 +33,7 @@ const MATERIAL_IMPORTS = [
 		...MATERIAL_IMPORTS,
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [ AppComponent ]
 })
 export class AppModule {
 }
