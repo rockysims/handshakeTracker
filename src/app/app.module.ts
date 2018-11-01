a9e8f8d811154c439015d3ecf05bda50bf99b71a
Yona Appletree <yona@concentricsky.com>
2018-10-31 18:04:38 -0700

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import { environment } from '../environments/environment';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { MatButtonModule, MatCheckboxModule } from "@angular/material";
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { environment } from "../environments/environment";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+
+const FIREBASE_IMPORTS = [
+	AngularFireModule.initializeApp(environment.firebase),
+	AngularFirestoreModule,
+	AngularFireAuthModule,
+];
+
+const MATERIAL_IMPORTS = [
+	MatButtonModule,
+	MatCheckboxModule,
+];
 
 @NgModule({
 	declarations: [
@@ -15,9 +28,9 @@ import {AppComponent} from './app.component';
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		AngularFireModule.initializeApp(environment.firebase),
-		AngularFirestoreModule,
-		AngularFireAuthModule
+		BrowserAnimationsModule,
+		...FIREBASE_IMPORTS,
+		...MATERIAL_IMPORTS,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
