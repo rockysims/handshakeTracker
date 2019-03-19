74069246ab57819d79bd57d974b992889da86bb6
Rocky Sims <rocky.sims@gmail.com>
2019-03-18 22:23:07 -0700

    Commented out everything in functions/src/index.ts temporarily.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,30 @@
-import * as functions from 'firebase-functions';
-
-export const helloWorld = functions.https.onRequest((req, res) => {
-	res.send("Hello from Firebase!");
-});
+// import * as functions from 'firebase-functions';
+// import * as algoliasearch from 'algoliasearch';
+//
+// const algoliaClient = algoliasearch(
+// 	functions.config().algolia.app_id,
+// 	functions.config().algolia.api_key
+// );
+//
+// const entryDoc = functions.firestore.document('/users/{userId}/entries/{entryId}');
+// exports.onEntryCreate = entryDoc.onCreate((snap, context) => {
+// 	return algoliaClient
+// 		.initIndex('entries')
+// 		.saveObject({
+// 			objectID: context.params.entryId,
+// 			...snap.data()
+// 		});
+// });
+// exports.onEntryUpdate = entryDoc.onUpdate((change, context) => {
+// 	return algoliaClient
+// 		.initIndex('entries')
+// 		.saveObject({
+// 			objectID: context.params.entryId,
+// 			...change.after!.data()
+// 		});
+// });
+// exports.onEntryDelete = entryDoc.onDelete((snap, context) => {
+// 	return algoliaClient
+// 		.initIndex('entries')
+// 		.deleteObject(context.params.entryId);
+// });
