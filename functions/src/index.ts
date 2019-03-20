b2075fdec021c438b68675447fcafe14b046ae8d
Rocky Sims <rocky.sims@gmail.com>
2019-03-19 22:32:40 -0700

    Theoretically finished writing firebase cloud functions to allow heroku server.ts to keep algolia index up to date with entries in firebase.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,13 +6,26 @@ admin.initializeApp(functions.config().firebase);
 const entryDoc = functions.firestore.document('/users/{userId}/entries/{entryId}');
 exports.onEntryCreate = entryDoc.onCreate((snap, context) => {
 	return snap.ref.update({
-		algoliaDirty: true
+		lastChangeTimestamp: Date.now(),
+		lastIndexTimestamp: 0
+	}).then(() => {
+		return updateLatestChangeTimestamp(context.params.userId);
 	});
 });
 exports.onEntryUpdate = entryDoc.onUpdate((change, context) => {
-	return change.after!.ref.update({
-		algoliaDirty: true
-	});
+	const before: any = change.before!.data();
+	const after: any = change.after!.data();
+	delete before['lastChangeTimestamp'];
+	delete after['lastChangeTimestamp'];
+	const reallyChanged = JSON.stringify(before) !== JSON.stringify(after);
+
+	if (reallyChanged) {
+		return change.after!.ref.update({
+			lastChangeTimestamp: Date.now()
+		}).then(() => {
+			return updateLatestChangeTimestamp(context.params.userId);
+		});
+	} else return;
 });
 exports.onEntryDelete = entryDoc.onDelete((snap, context) => {
 	const userId = context.params.userId;
@@ -20,42 +33,14 @@ exports.onEntryDelete = entryDoc.onDelete((snap, context) => {
 	return admin.firestore()
 		.collection(`/users/${userId}/deletedEntryIds`)
 		.doc(entryId)
-		.set({});
+		.set({})
+		.then(() => {
+			return updateLatestChangeTimestamp(userId);
+		});
 });
 
-
-
-
-
-
-
-
-// import * as algoliasearch from 'algoliasearch';
-//
-// const algoliaClient = algoliasearch(
-// 	functions.config().algolia.app_id,
-// 	functions.config().algolia.api_key
-// );
-//
-// const entryDoc = functions.firestore.document('/users/{userId}/entries/{entryId}');
-// exports.onEntryCreate = entryDoc.onCreate((snap, context) => {
-// 	return algoliaClient
-// 		.initIndex('entries')
-// 		.saveObject({
-// 			objectID: context.params.entryId,
-// 			...snap.data()
-// 		});
-// });
-// exports.onEntryUpdate = entryDoc.onUpdate((change, context) => {
-// 	return algoliaClient
-// 		.initIndex('entries')
-// 		.saveObject({
-// 			objectID: context.params.entryId,
-// 			...change.after!.data()
-// 		});
-// });
-// exports.onEntryDelete = entryDoc.onDelete((snap, context) => {
-// 	return algoliaClient
-// 		.initIndex('entries')
-// 		.deleteObject(context.params.entryId);
-// });
+function updateLatestChangeTimestamp(userId: string) {
+	return admin.firestore().collection(`/users/${userId}/latest`).doc('change').set({
+		timestamp: Date.now()
+	});
+}
